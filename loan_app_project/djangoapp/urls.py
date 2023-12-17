2af8ba844af0cdddb64ae5a40c3e040615e9e205
Shanki200801 <shashank200801@gmail.com>
2023-12-17 22:16:45 +0530

    changed up the URLs a bit

--- loan_app_project/djangoapp/urls.py
+++ loan_app_project/djangoapp/urls.py
@@ -2,11 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.getUsers),
-    path("create", views.addUser),
-    path("read/<str:pk>", views.getUser),
-    path("update/<str:pk>", views.updateUser),
-    path("delete/<str:pk>", views.deleteUser),
     path("register", views.registerCustomer),
     path("getcustomer/<str:pk>", views.getCustomer),
     path("loanid/<str:pk>", views.getLoanByLoanId),
--- loan_app_project/loan_app_project/urls.py
+++ loan_app_project/loan_app_project/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("users/", include("djangoapp.urls")),
+    path("admin-site/", admin.site.urls),
+    path("", include("djangoapp.urls")),
 ]
